0df626b1d8632445ddeb94b285aa8471558c9927
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-03-05 12:50:56 +0300

    Replace hard code with vars

--- deploy_maas.sh
+++ deploy_maas.sh
@@ -4,6 +4,20 @@ set -o pipefail
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
+MAAS_ADMIN=${MAAS_ADMIN:-"admin"}
+MAAS_PASS=${MAAS_PASS:-"admin"}
+MAAS_ADMIN_MAIL=${MAAS_ADMIN_MAIL:-"admin@maas.tld"}
+UPSTREAM_DNS="8.8.8.8"
+IPMI_IPS="192.168.50.20 192.168.50.21 192.168.50.22 192.168.50.23 192.168.50.24"
+IPMI_USER="ADMIN"
+IPMI_PASS="ADMIN"
+IPMI_POWER_DRIVER="LAN_2_0"
+DHCP_RESERVATION=("192.168.50.100" "192.168.50.255")
+
+# determined variables
+DISTRO=$(cat /etc/*release | egrep '^ID=' | awk -F= '{print $2}' | tr -d \")
+NODE_IP=`ip addr show dev $PHYS_INT | grep 'inet ' | awk '{print $2}' | head -n 1 | cut -d '/' -f 1`
+
 function set_ssh_keys() {
   [ ! -d ~/.ssh ] && mkdir ~/.ssh && chmod 0700 ~/.ssh
   [ ! -f ~/.ssh/id_rsa ] && ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ''
@@ -11,40 +25,42 @@ function set_ssh_keys() {
   grep "$(<~/.ssh/id_rsa.pub)" ~/.ssh/authorized_keys -q || cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 }
 
+# Install MAAS 2.7
 sudo apt update
-sudo apt-get install snapd lsof -y
+sudo apt-get install snapd jq -y
 sudo snap install maas --channel=2.7
 sudo maas init --mode all \
-    --maas-url "http://192.168.50.5:5240/MAAS" \
-    --admin-username admin \
-    --admin-password 12349876 \
-    --admin-email ${USER}@domain.ltd
+    --maas-url "http://${NODE_IP}:5240/MAAS" \
+    --admin-username ${MAAS_ADMIN} \
+    --admin-password ${MAAS_PASS} \
+    --admin-email ${MAAS_ADMIN_MAIL}
 
-#login
+# login
 export PATH=$PATH:/snap/bin
 PROFILE="admin"
-MAAS_URL="http://192.168.50.5:5240/MAAS/api/2.0"
+MAAS_URL="http://${NODE_IP}:5240/MAAS/api/2.0"
 API_KEY_FILE=api_key
 sudo maas apikey --username=$PROFILE > $API_KEY_FILE
 maas login $PROFILE $MAAS_URL - < $API_KEY_FILE
+
+# Add public key to user "admin"
 set_ssh_keys
 SSH_KEY=$(cat ~/.ssh/id_rsa.pub)
 maas $PROFILE sshkeys create "key=$SSH_KEY"
 
-#config values
-MY_UPSTREAM_DNS="8.8.8.8"
-maas $PROFILE maas set-config name=upstream_dns value=$MY_UPSTREAM_DNS
+# Configure dns
+maas $PROFILE maas set-config name=upstream_dns value=$UPSTREAM_DNS
 
-
+# Configure dhcp
 maas $PROFILE ipranges create type=dynamic \
-    start_ip=192.168.50.100 end_ip=192.168.50.100 \
+    start_ip=${DHCP_RESERVATION[0]} end_ip=${DHCP_RESERVATION[1]} \
     comment='This is a reserved dynamic range'
-
 maas $PROFILE vlan update 0 0 dhcp_on=True primary_rack=maas-dev
 
+# start import default images
 maas $PROFILE boot-resources import
 
-# Waiting for image downoad to complete
+# Waiting for default images downoad to complete
 i=0
 while [ $i -le 30 ] ; do
   sleep 20
@@ -55,11 +71,13 @@ while [ $i -le 30 ] ; do
 done
 sleep 30
 
-maas $PROFILE machines create \
-    architecture="amd64/generic" \
-    hwe_kernel="ga-18.04" \
-    power_type="ipmi" \
-    power_parameters_power_driver=LAN_2_0 \
-    power_parameters_power_user=ADMIN \
-    power_parameters_power_pass=ADMIN \
-    power_parameters_power_address=192.168.50.21
+for n in $IPMI_IPS ; do 
+  maas $PROFILE machines create \
+      architecture="amd64/generic" \
+      hwe_kernel="ga-18.04" \
+      power_type="ipmi" \
+      power_parameters_power_driver=${IPMI_POWER_DRIVER} \
+      power_parameters_power_user=${IPMI_USER} \
+      power_parameters_power_pass=${IPMI_PASS} \
+      power_parameters_power_address=${n}
+done
